096e754db4241997b33e2758a7d53baae40bf9e5
alecbiela <alec.bielanos@gmail.com>
2024-07-24 11:02:00 -0400

    Add petite-vue templates for some content

--- build/gruntfile.js
+++ build/gruntfile.js
@@ -1,25 +1,25 @@
 /* Derived from https://gist.github.com/alexilyaev/cf7d56f105046400fcc8 */
 
 module.exports = function (grunt) {
-  "use strict";
+  'use strict'
 
-  const sass = require("sass");
-  require("load-grunt-tasks")(grunt);
+  const sass = require('sass')
+  require('load-grunt-tasks')(grunt)
 
   // Project configuration.
   grunt.initConfig({
     // Read settings from package.json
-    pkg: grunt.file.readJSON("package.json"),
+    pkg: grunt.file.readJSON('package.json'),
     // Paths settings
     dirs: {
       src: {
-        sass: "./css/sass",
-        css: "./css",
-        js: "./js",
+        sass: './css/sass',
+        css: './css',
+        js: './js',
       },
       dest: {
-        css: "../css",
-        js: "../js",
+        css: '../css',
+        js: '../js',
       },
     },
     // Check that all JS files conform to our `.jshintrc` files
@@ -28,18 +28,18 @@ module.exports = function (grunt) {
         jshintrc: true,
       },
       target: {
-        src: "<%= dirs.src.js %>/**/*.js",
+        src: '<%= dirs.src.js %>/**/*.js',
       },
     },
     sass: {
       options: {
         implementation: sass,
-        outputStyle: "compressed",
+        outputStyle: 'compressed',
         sourceMap: true,
       },
       target: {
-        src: "<%= dirs.src.sass %>/main.scss",
-        dest: "<%= dirs.src.css %>/main.css",
+        src: '<%= dirs.src.sass %>/main.scss',
+        dest: '<%= dirs.src.css %>/main.css',
       },
     },
     // Combine CSS
@@ -52,10 +52,10 @@ module.exports = function (grunt) {
           {
             expand: true,
             useGzip: true,
-            cwd: "<%= dirs.src.css %>",
-            src: ["main.css"],
-            dest: "<%= dirs.dest.css %>",
-            ext: ".min.css",
+            cwd: '<%= dirs.src.css %>',
+            src: ['main.css'],
+            dest: '<%= dirs.dest.css %>',
+            ext: '.min.css',
           },
         ],
       },
@@ -64,15 +64,15 @@ module.exports = function (grunt) {
     uglify: {
       options: {
         banner:
-          "/*! <%= pkg.name %> " +
+          '/*! <%= pkg.name %> ' +
           '<%= grunt.template.today("dd-mm-yyyy") %> */\n',
         compress: true,
         mangle: true,
         sourceMap: false, //change to true for dev
       },
       target: {
-        src: ["<%= dirs.src.js %>/main.js"],
-        dest: "<%= dirs.dest.js %>/main.min.js",
+        src: ['<%= dirs.src.js %>/main.js'],
+        dest: '<%= dirs.dest.js %>/main.min.js',
       },
     },
     // Trigger relevant tasks when the files they watch has been changed
@@ -82,24 +82,24 @@ module.exports = function (grunt) {
         options: {
           reload: true,
         },
-        files: ["Gruntfile.js", "package.json"],
+        files: ['Gruntfile.js', 'package.json'],
       },
       css: {
-        files: "<%= dirs.src.css %>/sass/**/*.scss",
-        tasks: ["build-css"],
+        files: '<%= dirs.src.css %>/sass/**/*.scss',
+        tasks: ['build-css'],
       },
       js: {
-        files: "<%= dirs.src.js %>/**/*.js",
-        tasks: ["build-js"],
+        files: '<%= dirs.src.js %>/**/*.js',
+        tasks: ['build-js'],
       },
     },
-  });
+  })
 
   // Setup build tasks aliases
-  grunt.registerTask("build-js", ["jshint", "uglify"]);
-  grunt.registerTask("build-css", ["sass", "cssmin"]);
-  grunt.registerTask("build", ["build-js", "build-css"]);
+  grunt.registerTask('build-js', ['jshint', 'uglify'])
+  grunt.registerTask('build-css', ['sass', 'cssmin'])
+  grunt.registerTask('build', ['build-js', 'build-css'])
 
   // Default task(s).
-  grunt.registerTask("default", ["build"]);
-};
+  grunt.registerTask('default', ['build'])
+}
--- build/js/main.js
+++ build/js/main.js
@@ -1,126 +1,149 @@
-import { createApp, reactive } from "https://unpkg.com/petite-vue?module";
-import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
-import DOMPurify from "https://cdn.jsdelivr.net/npm/dompurify@3.1.6/dist/purify.es.mjs";
+import { createApp, reactive } from 'https://unpkg.com/petite-vue?module'
+// import { marked } from 'https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js'
+// import DOMPurify from 'https://cdn.jsdelivr.net/npm/dompurify@3.1.6/dist/purify.es.mjs'
 
 window.onload = (() => {
-  "use strict";
+  'use strict'
 
-  /* -- Removed light/dark mode switching for now --
-    let isLight = !(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
-    const html = document.documentElement;
-    const switchTheme = document.getElementById('theme_switcher');
-    const os_default = '<svg viewBox="0 0 16 16"><path fill="currentColor" d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/></svg>';
-    const sun = '<svg viewBox="0 0 16 16"><path fill="currentColor" d="M8 11a3 3 0 1 1 0-6a3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8a4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/></svg>';
-    const moon = '<svg viewBox="0 0 16 16"><g fill="currentColor"><path d="M6 .278a.768.768 0 0 1 .08.858a7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277c.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316a.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71C0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278zM4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063c-4.61 0-8.343-3.714-8.343-8.29c0-1.167.242-2.278.681-3.286z"/><path d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"/></g></svg>';
-    switchTheme.addEventListener('click', (e)=> {
-        e.preventDefault();
-        isLight = !isLight;
-        html.setAttribute('data-theme', isLight? 'light':'dark');
-        switchTheme.innerHTML = isLight? sun : moon;
-        switchTheme.setAttribute('data-tooltip', `${isLight?'Light':'Dark'} Mode`);
-        removeTooltip();
-    });
-    const removeTooltip = (timeInt = 1750) => {
-        setTimeout(()=>{
-            switchTheme.blur();
-        },timeInt);
-    };
-    
-    switchTheme.innerHTML = os_default;
-    switchTheme.setAttribute('data-tooltip', 'Auto Theme');
-    switchTheme.focus();
-    removeTooltip(3000);
-    */
+  // Converts strings from snake case to title case
+  const getTitleCase = (str) => {
+    return str
+      .match(
+        /[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g
+      )
+      .map((x) => x.slice(0, 1).toUpperCase() + x.slice(1))
+      .join(' ')
+  }
+
   // Global store for the entire page
   const store = reactive({
     count: 0,
     navOpen: false,
-    content: { about: "" },
-  });
+    content: {},
+    util: { getTitleCase },
+  })
 
   // Load up all the content from the markdown files
-  const asyncGetContent = async (url) => {
+  // const asyncGetMarkdown = async (url) => {
+  //   try {
+  //     const res = await fetch(url, {
+  //       headers: {
+  //         'Content-Type': 'text/plain',
+  //       },
+  //     })
+  //     if (!res.ok) {
+  //       throw new Error(
+  //         `Getting content failed at URL ${url} with error status ${res.status}`
+  //       )
+  //     }
+  //     const text = await res.text()
+  //     return DOMPurify.sanitize(marked(text))
+  //   } catch (error) {
+  //     console.error(error.message)
+  //   }
+  // }
+
+  // Load up all the content from the markdown files
+  const asyncGetJSON = async (url) => {
     try {
       const res = await fetch(url, {
         headers: {
-          "Content-Type": "text/plain",
+          'Content-Type': 'application/json',
         },
-      });
+      })
       if (!res.ok) {
         throw new Error(
-          `Getting content failed at URL ${url} with error status ${res.status}`
-        );
+          `Getting JSON failed at URL ${url} with error status ${res.status}`
+        )
       }
-      const text = await res.text();
-      return DOMPurify.sanitize(marked(text));
+      return await res.json()
     } catch (error) {
-      console.error(error.message);
+      console.error(error.message)
     }
-  };
+  }
 
   const loadContent = async () => {
-    await asyncGetContent("/md/about.md").then(
-      (text) => (store.content.about = text)
-    );
-  };
+    let promises = []
+    // promises.push(
+    //   await asyncGetMarkdown('/content/about.md').then(
+    //     (text) => (store.content.about = text)
+    //   )
+    // )
+    promises.push(
+      await asyncGetJSON('/content/my_stack.json').then(
+        (json) => (store.content.my_stack = json)
+      )
+    )
+    promises.push(
+      await asyncGetJSON('/content/academic_work.json').then(
+        (json) => (store.content.academic_work = json)
+      )
+    )
+    promises.push(
+      await asyncGetJSON('/content/personal_projects.json').then(
+        (json) => (store.content.personal_projects = json)
+      )
+    )
+    Promise.all(promises).then(() => document.body.classList.remove('loading'))
+  }
 
   // Set up smooth scrolling on all anchor links
   const smoothScroll = () => {
-    let x = document.querySelectorAll('a[href*="#"]');
+    let x = document.querySelectorAll('a[href*="#"]')
     for (let i = 0; i < x.length; i++) {
       x[i].onclick = (e) => {
-        e.preventDefault();
-        const ele = document.querySelector(e.target.hash);
+        e.preventDefault()
+        const ele = document.querySelector(e.target.hash)
         window.scrollTo({
           top: ele.offsetTop - 90,
           left: 0,
-          behavior: "smooth",
-        });
+          behavior: 'smooth',
+        })
         document
-          .getElementById("nav_toggle")
-          .setAttribute("aria-expanded", "false");
-        ele.focus();
-        return false;
-      };
+          .getElementById('nav_toggle')
+          .setAttribute('aria-expanded', 'false')
+        ele.focus()
+        return false
+      }
     }
-  };
+  }
 
   // Main initialization logic, runs when petite-vue gets mounted
   const handleOnMount = () => {
     //set honeypot
-    document.getElementById("hhpp").value = "check";
+    document.getElementById('hhpp').value = 'check'
 
     //init smooth scrolling functionality
-    smoothScroll();
+    smoothScroll()
 
     //get page content
-    loadContent();
-  };
+    loadContent()
+  }
 
   // Form submit for social links
   const handleSocialLinks = (e) => {
-    if (document.getElementById("hhpp").value === "check") {
+    if (document.getElementById('hhpp').value === 'check') {
       switch (e.submitter.id) {
-        case "bt_em":
-          window.open("mailto:alec.bielanos@gmail.com", "_blank");
-          break;
-        case "bt_li":
-          window.open("https://www.linkedin.com/in/alec-bielanos/", "_blank");
-          break;
-        case "bt_gh":
+        case 'bt_em':
+          window.open('mailto:alec.bielanos@gmail.com', '_blank')
+          break
+        case 'bt_li':
+          window.open('https://www.linkedin.com/in/alec-bielanos/', '_blank')
+          break
+        case 'bt_gh':
           window.open(
-            "https://bitbucket.org/alecbiela/workspace/repositories/",
-            "_blank"
-          );
-          break;
+            'https://bitbucket.org/alecbiela/workspace/repositories/',
+            '_blank'
+          )
+          break
       }
     }
-  };
+  }
 
   createApp({
     // share it with app scopes
     store,
     handleSocialLinks,
     handleOnMount,
-  }).mount();
-})();
+  }).mount()
+})()
